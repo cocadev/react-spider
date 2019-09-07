15f6506b6f2994372549ef77900c0facc492a2cf
Wang Harry <40970351+funnyjerry@users.noreply.github.com>
2019-09-07 20:23:20 +0800

    remove demo files

--- src/index.js
+++ src/index.js
@@ -6,10 +6,6 @@ import { ThemeProvider } from '@material-ui/styles';
 import theme from './src/utils/responsive'
 import "assets/scss/material-kit-react.scss?v=1.8.0";
 
-// pages for this product
-import Components from "views/Components/Components.js";
-import ProfilePage from "views/ProfilePage/ProfilePage.js";
-
 import Home from "./src/view/Home";
 import AppTutorials from "./src/view/AppTutorials";
 import OurApps from "./src/view/OurApps";
@@ -24,8 +20,6 @@ ReactDOM.render(
   <Router history={hist}>
     <ThemeProvider theme={theme}>
       <Switch>
-        <Route path="/guide" component={Components} />
-        <Route path="/profile-page" component={ProfilePage} />
         <Route path="/apps" component={OurApps} />
         <Route path="/price" component={Price} />
         <Route path="/tutorial" component={AppTutorials} />
